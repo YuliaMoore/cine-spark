e4c0d6fed892d1d882b8c8137ebed9b8db0a1460
Vetik007 <boy0007@ukr.net>
2023-05-08 11:36:53 +0300

    add style

--- src/js/MoviesAPI.js
+++ src/js/MoviesAPI.js
@@ -1,4 +1,122 @@
-// підключаємо бібліотеку axios
+// // підключаємо бібліотеку axios
+// import axios from 'axios';
+
+// export class MoviesAPI {
+//   // Приватні властивості
+//   #BASE_URL = 'https://api.themoviedb.org';
+//   #API_KEY = 'cca79e12b25cdd9f67fc795a1689f5d9';
+//   #query = '';
+
+//   //для відображення рандомних фільмів у каталозі
+//   getTrendingFilms(page) {
+//     return axios.get('${this.#BASE_URL}/3/trending/movie', {
+//       params: {
+//         query: 'random',
+//         page,
+//         per_page: 10,
+//         movie_id: this.#API_KEY,
+//       },
+//     });
+//   }
+
+//   // для відображення фільмів у каталозі за пошуком
+//   fetchFilmsByQuery(page) {
+//     return axios.get(`${this.#BASE_URL}/search/movie`, {
+//       params: {
+//         query: this.#query,
+//         page,
+//         per_page: 10,
+//         movie_id: this.#API_KEY,
+//       },
+//     });
+//   }
+
+//   set query(newQuery) {
+//     this.#query = newQuery;
+//   }
+
+//   // Романчук Володимир "ставлю свої поки що, потім розберемось"...
+//   // Три функції: тренди дня, тижня і пошуковий запит.
+
+//   // Лучкевич Віталій додав функцію запиту нових фільмів getUpcomingFilms
+
+//   async getTrendMoviesDay() {
+//     try {
+//       const response = await axios.get(
+//         `${this.#BASE_URL}/3/trending/movie/day?api_key=${this.#API_KEY}`
+//       );
+//       return response.data;
+//     } catch (error) {
+//       throw new Error(error.response.status);
+//     }
+//   }
+
+//   async getTrendMoviesWeek() {
+//     try {
+//       const response = await axios.get(
+//         `${this.#BASE_URL}/3/trending/movie/week?api_key=${
+//           this.#API_KEY
+//         }&append_to_response=videos`
+//       );
+//       return response.data;
+//     } catch (error) {
+//       throw new Error(error.response.status);
+//     }
+//   }
+
+//   async getUpcomingFilms() {
+//     try {
+//       const response = await axios.get(
+//         `${this.#BASE_URL}/3/movie/upcoming?api_key=${this.#API_KEY}`
+//       );
+//       return response.data;
+//     } catch (error) {
+//       throw new Error(error.response.status);
+//     }
+//   }
+
+//   async getSearchMovies(searchQuery, page) {
+//     try {
+//       const response = await axios.get(
+//         `${this.#BASE_URL}/3/search/movie?api_key=${
+//           this.#API_KEY
+//         }&query=${searchQuery}&language=en-US&page=${page}&include_adult=false`
+//       );
+//       return response.data;
+//     } catch (error) {
+//       throw new Error(error.response.status);
+//     }
+//   }
+
+//   // Функція для отримання 2-х жанрів фільмів по вказаних ids
+//   // Але зробити не вдалось, оскільки з Handlesbar не підтримує асинхронні функції в хелперах, тому звичайний масив локатоно зберігаємо.
+//   // async getGenres(...ids) {
+//   //   try {
+//   //     const response = await axios.get(
+//   //       `${this.#BASE_URL}/3/genre/movie/list?api_key=${
+//   //         this.#API_KEY
+//   //       }&language=en-US`
+//   //     );
+//   //     const genres = response.data.genres.filter(genre =>
+//   //       ids.includes(genre.id)
+//   //     );
+//   //     let genresText = '';
+//   //     // console.log(genres.length);
+//   //     if (genres.length > 1) {
+//   //       genresText = `${genres[0].name}, ${genres[1].name}`;
+//   //     } else {
+//   //       genresText = `${genres[0].name}`;
+//   //     }
+//   //     // console.log(genresText);
+//   //     return genresText;
+//   //   } catch (error) {
+//   //     throw new Error(error.response.status);
+//   //   }
+//   // }
+// }
+
+// !==================================================
+
 import axios from 'axios';
 
 export class MoviesAPI {
@@ -7,83 +125,68 @@ export class MoviesAPI {
   #API_KEY = 'cca79e12b25cdd9f67fc795a1689f5d9';
   #query = '';
 
-  //для відображення рандомних фільмів у каталозі
-  getTrendingFilms(page) {
-    return axios.get('${this.#BASE_URL}/3/trending/movie', {
-      params: {
-        query: 'random',
-        page,
-        per_page: 10,
-        movie_id: this.#API_KEY,
-      },
-    });
-  }
+  // //для відображення рандомних фільмів у каталозі
+  // getTrendingFilms(page) {
+  //   return axios.get('${this.#BASE_URL}/3/trending/movie', {
+  //     params: {
+  //       query: 'random',
+  //       page,
+  //       per_page: 10,
+  //       movie_id: this.#API_KEY,
+  //     },
+  //   });
+  // }
 
-  // для відображення фільмів у каталозі за пошуком
-  fetchFilmsByQuery(page) {
-    return axios.get(`${this.#BASE_URL}/search/movie`, {
-      params: {
-        query: this.#query,
-        page,
-        per_page: 10,
-        movie_id: this.#API_KEY,
-      },
-    });
-  }
+  // // для відображення фільмів у каталозі за пошуком
+  // fetchFilmsByQuery(page) {
+  //   return axios.get(`${this.#BASE_URL}/search/movie`, {
+  //     params: {
+  //       query: this.#query,
+  //       page,
+  //       per_page: 10,
+  //       movie_id: this.#API_KEY,
+  //     },
+  //   });
+  // }
 
   set query(newQuery) {
     this.#query = newQuery;
   }
 
-  // Романчук Володимир "ставлю свої поки що, потім розберемось"...
-  // Три функції: тренди дня, тижня і пошуковий запит.
-
-  // Лучкевич Віталій додав функцію запиту нових фільмів getUpcomingFilms
+  // Отримати тренди дня
 
   async getTrendMoviesDay() {
-    try {
-      const response = await axios.get(
-        `${this.#BASE_URL}/3/trending/movie/day?api_key=${this.#API_KEY}`
-      );
-      return response.data;
-    } catch (error) {
-      throw new Error(error.response.status);
-    }
+    const response = await axios.get(
+      `${this.#BASE_URL}/3/trending/movie/day?api_key=${this.#API_KEY}`
+    );
+    return response.data;
   }
 
+  // Отримати тренди тижня
   async getTrendMoviesWeek() {
-    try {
-      const response = await axios.get(
-        `${this.#BASE_URL}/3/trending/movie/week?api_key=${this.#API_KEY}`
-      );
-      return response.data;
-    } catch (error) {
-      throw new Error(error.response.status);
-    }
+    const response = await axios.get(
+      `${this.#BASE_URL}/3/trending/movie/week?api_key=${this.#API_KEY}`
+    );
+    return response.data;
   }
 
+  // отримати нові фільми
   async getUpcomingFilms() {
-    try {
-      const response = await axios.get(
-        `${this.#BASE_URL}/3/movie/upcoming?api_key=${this.#API_KEY}`
-      );
-      return response.data;
-    } catch (error) {
-      throw new Error(error.response.status);
-    }
+    const response = await axios.get(
+      `${this.#BASE_URL}/3/movie/upcoming?api_key=${this.#API_KEY}`
+    );
+    return response.data;
   }
+
+  // Отримати фільми по запиту
 
   async getSearchMovies(searchQuery, page) {
-    try {
-      const response = await axios.get(
-        `${this.#BASE_URL}/3/search/movie?api_key=${
-          this.#API_KEY
-        }&query=${searchQuery}&language=en-US&page=${page}&include_adult=false`
-      );
-      return response.data;
-    } catch (error) {
-      throw new Error(error.response.status);
-    }
+    const response = await axios.get(
+      `${this.#BASE_URL}/3/search/movie?api_key=${
+        this.#API_KEY
+      }&query=${searchQuery}&language=en-US&page=${page}&include_adult=false`
+    );
+    return response.data;
   }
 
   // Функція для отримання 2-х жанрів фільмів по вказаних ids
--- src/js/homeWeekly.js
+++ src/js/homeWeekly.js
@@ -1,207 +1,58 @@
-// import { getTrendingFilms } from './MoviesAPI';
-
 import { MoviesAPI } from './MoviesAPI';
 const moviesAPI = new MoviesAPI();
 
-const weeklyGallery = document.querySelector('.weekly-card');
+import { createMovies } from '/src/js/catalog-functions/weekly-markup';
 
-// const moviesApi = new MoviesAPI();
-// console.log(moviesApi);
-
-const genres = [
-  { id: 28, name: 'Action' },
-  { id: 12, name: 'Adventure' },
-  { id: 16, name: 'Animation' },
-  { id: 35, name: 'Comedy' },
-  { id: 80, name: 'Crime' },
-  { id: 99, name: 'Documentary' },
-  { id: 18, name: 'Drama' },
-  { id: 10751, name: 'Family' },
-  { id: 14, name: 'Fantasy' },
-  { id: 36, name: 'History' },
-  { id: 27, name: 'Horror' },
-  { id: 10402, name: 'Music' },
-  { id: 9648, name: 'Mystery' },
-  { id: 10749, name: 'Romance' },
-  { id: 878, name: 'Science Fiction' },
-  { id: 10770, name: 'TV Movie' },
-  { id: 53, name: 'Thriller' },
-  { id: 10752, name: 'War' },
-  { id: 37, name: 'Western' },
-];
+const weeklyGallery = document.querySelector('.weekly-list');
 
 async function onRenderPage() {
   try {
     const respons = await moviesAPI.getTrendMoviesWeek();
-    console.log(respons);
+    // console.log(respons);
 
     const responsData = respons.results;
-    console.log(responsData);
-    console.log(responsData.length);
+    // console.log(responsData.movie_id);
 
+    // ================================
     let responsMovies = [];
 
-    getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
+    const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
 
     while (responsMovies.length != 3) {
       let index = getRandomInt(responsData.length);
       responsMovies.push(responsData[index]);
       responsMovies = responsMovies.filter((v, i, arr) => arr.indexOf(v) == i);
     }
+    // ===================================
 
     const markup = responsMovies.reduce(
       (markup, responsMovies) => markup + createMovies(responsMovies),
       ''
     );
 
+    // console.log(markup.length);
+
     updateNewsList(markup);
+
+    // // =======================================
+    // function RandArray(array) {
+    //   var rand = (Math.random() * array.length) | 3;
+    //   var rValue = array[rand];
+    //   return rValue;
+    // }
+
+    // // var myArray = ['one', 'two', 'three', 'four', 'five', 'six'];
+    // var rValue = RandArray(responsData);
+    // console.log(rValue.length);
+
+    // // =====================================
   } catch (err) {
     console.log(err);
   }
 }
-
-console.log(onRenderPage());
-
-function createMovies({
-  title,
-  release_date,
-  poster_path,
-  genre_ids,
-  vote_average,
-}) {
-  let year = '';
-  if (release_date) {
-    year = release_date?.slice(0, 4);
-  } else {
-    year = first_air_date?.slice(0, 4);
-  }
-
-  let tempGenres = [];
-
-  genre_ids.forEach(genre => {
-    genres.forEach(({ id, name }) => {
-      if (id === genre) {
-        tempGenres.push(name);
-        // (tempGenres);
-      }
-    });
-  });
-  let tempGenresString = tempGenres.join(', ');
-
-  return `
-    
-  <li class="weekly-item">
-  <a href="#" class="weekly-link">
-    <div class="weekly-wrapper">
-
-      <div class="weekly-info">
-        <h2 class="weekly-info__title">${title}</h2>
-
-        <div class="catalog-list__additional-info">
-          <p class="weekly-info__genres__data">${tempGenresString} | ${release_date.slice(
-    0,
-    4
-  )}
-        </p>
-
-          <div class="weekly-rating">${vote_average}</div>
-        </div>
-      </div>
-
-      <img
-        src="https://image.tmdb.org/t/p/w500${poster_path}"
-        alt="${title}"
-        
-        class="weekly-img"
-      />
-    </div>
-  </a>
-</li>
-
-  
-
-`;
-}
+onRenderPage();
+// console.log(onRenderPage());
 
 function updateNewsList(markup) {
   weeklyGallery.innerHTML = markup;
-}
-
-// !=======================================
-
-{
-  /* <li class="catalog-list__item">
-  <a href="#" class="catalog-list__list-link">
-    <div class="catalog-list__list-wrapper">
-      <div class="catalog-list__info">
-        <h2 class="catalog-list__title">${title}</h2>
-        <div class="catalog-list__additional-info">
-          <p class="catalog-list__movie-type">
-            ${getGenres(genre_ids)} | ${release_date.slice(0, 4)}
-          </p>
-          <div class="catalog-list-rating">${getStarsRating(vote_average)}</div>
-        </div>
-      </div>
-      <img
-        src="https://image.tmdb.org/t/p/w500${poster_path}"
-        alt="${title}"
-        width="280"
-        height="406"
-        class="catalog-list__image"
-      />
-    </div>
-  </a>
-</li>; */
-}
-
-// !======================================
-
-{
-  /* <ul class="weekly-list">
-  <a href="#">
-    <li class="catalog-list__item weekly-item">
-      <img
-        src="https://image.tmdb.org/t/p/w500${poster_path}"
-        alt="${title}"
-        width="280"
-        height="406"
-        class="catalog-list__image weekly-img"
-      />
-      <div class="catalog-list__info weekly-info">
-        <h2 class="catalog-list__title weekly-info__title">${title}</h2>
-        <p class="catalog-list__movie-type weekly-info__genres">
-          ${tempGenresString}
-        </p>
-        <p class="catalog-list__movie-year weekly-info__date">${year}</p>
-      </div>
-      <div class="catalog-list__movie-rating weekly-rating">
-        ${vote_average}
-      </div>
-    </li>
-  </a>
-</ul>; */
-}
-
-// ==============================
-
-{
-  // <ul class="weekly-list">
-  //! <a href="#">
-  //! <li class="weekly-item">
-  // <div class="rrr">
-  //! <img
-  //! src="https://image.tmdb.org/t/p/w500${poster_path}"
-  //! alt="${title}"
-  //! class="weekly-img"
-  // />
-  //! <div class="weekly-info">
-  //! <h2 class="weekly-info__title">${title}</h2>
-  //! <p class="weekly-info__genres">${tempGenresString}</p>
-  //* <p class="weekly-info__date">${year}</p>
-  // </div>
-  //! <div class="weekly-rating">${vote_average}</div>
-  // </div>
-  // </li>
-  // </a>
-  // </ul>;
 }
