5b043905b5d5cb7d645f9eeef463e40e1c98c53b
kirsland <116314251+kirsland@users.noreply.github.com>
2023-05-12 10:04:41 +0300

    2023-library-mistake-fix

--- src/js/modal-window/modal-movie.js
+++ src/js/modal-window/modal-movie.js
@@ -2,6 +2,7 @@ import symboldefs from '../../images/symbol-defs.svg';
 import { MoviesAPI } from '../MoviesAPI';
 import { addToLibrary } from './add-to-library';
 import { removeFromLibrary } from './remove-from-my-library';
+import { onRenderLibraryCards } from '../library';
 
 const modalEl = document.querySelector('.modal-card');
 
@@ -67,19 +68,31 @@ export async function openModalMovie(id) {
         // console.log('Фільм є в Locale Storage');
         // console.log('Нова назва кнопки + слухач з функцією видалення.');
         addRemoveLibraryButton.textContent = 'Remove from library';
+        addRemoveLibraryButton.classList.add('remove');
         addRemoveLibraryButton.addEventListener('click', function () {
           removeFromLibrary(movieObject.id);
           // console.log('Видалили з Locale Storage.');
+          // Якщо ми на сторінці my-library - запускаємо новий рендер сторінки onRenderLibraryCards()
+          if (window.location.pathname.includes('my-library')) {
+            // console.log('My-library is OPENED');
+            onRenderLibraryCards();
+          }
           modalButtonCreation();
         });
       } else {
         // console.log('Фільму немає в Locale Storage');
         // console.log('Нова назва кноки іслухач з функцію додавання');
         addRemoveLibraryButton.textContent = 'Add to library';
+        addRemoveLibraryButton.classList.remove('remove');
         addRemoveLibraryButton.addEventListener('click', function () {
           addToLibrary(movieObject);
           // console.log(`movieObject (from modal-movie.js)`, movieObject);
           // console.log('Вийшов з функції додавання Locale Storage', movieObject);
+          // Якщо ми на сторінці my-library - запускаємо новий рендер сторінки onRenderLibraryCards()
+          if (window.location.pathname.includes('my-library')) {
+            // console.log('My-library is OPENED');
+            onRenderLibraryCards();
+          }
           modalButtonCreation();
         });
       }
