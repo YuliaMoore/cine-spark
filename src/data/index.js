541e5dd680697b21f6ee5259315c6a878ee2ecb1
Yulia Tkach <115689321+YuliaMoore@users.noreply.github.com>
2023-05-12 18:48:39 +0300

    Add files via upload

---
+++ src/data/index.js
@@ -0,0 +1,8 @@
+import './js/color-theme';
+// import './js/hero';
+import './js/homeWeekly';
+import './js/homeUpcoming';
+import './js/modal';
+import './js/mobile-menu';
+import './js/movie-card';
+import './js/spiner';
