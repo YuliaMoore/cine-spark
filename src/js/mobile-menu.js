cfa316576bd24bc1158e8a97cfa95a657149b04b
SerhiiOd <115508914+SerhiiOd@users.noreply.github.com>
2023-05-12 13:18:08 +0300

    upd-js/mob-menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -6,10 +6,27 @@
     body: document.querySelector('body'),
   };
   refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.body.addEventListener('click', closeMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
     refs.body.classList.toggle('no-scroll');
     refs.body.classList.toggle('has-backdrop');
   }
+
+  function closeMenu(event) {
+    if (!event.target.closest('.mob-menu') && !event.target.closest('.menu-open-btn')) {
+      refs.menu.classList.add('is-hidden');
+      refs.body.classList.remove('no-scroll');
+      refs.body.classList.remove('has-backdrop');
+    }
+  }
+
+  function onKeyPress(event) {
+    if (event.code === 'Escape') {
+      toggleMenu();
+    }
+  }
+
+  document.addEventListener('keydown', onKeyPress);
 })();
