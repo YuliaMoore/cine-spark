e0ef986dec07f4a47c6e5b76667ffe37bdaa1c3c
SerhiiOd <115508914+SerhiiOd@users.noreply.github.com>
2023-05-12 18:43:32 +0300

    sdsdsds

--- src/js/catalog-functions/search-form-update.js
+++
@@ -1,7 +0,0 @@
-// import { replaceSearchForm } from './search-form-markup';
-
-// export function searchFormUpdate() {
-//   //   console.log('Викликаю імпортовану функцію replaceSearchForm(), яка');
-//   //   console.log('підвантажує три Селекти, опціями з АПІ або локальних файлів');
-//   replaceSearchForm();
-// }
--- src/my-library.js
+++
@@ -1,8 +0,0 @@
-import './js/color-theme';
-import './js/modal';
-import './js/mobile-menu';
-import './js/library';
-import './js/movie-card';
-import './js/scroll';
-import './js/spiner';
-import './js/hero-modal';
